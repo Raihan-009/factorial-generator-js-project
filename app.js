1c3605978d3227b1c70e5dc939438cc2d3149b03
Raihan Islam <64744693+Raihan-009@users.noreply.github.com>
2022-04-09 18:06:35 +0600

    disable button added

--- app.js
+++ app.js
@@ -1,30 +1,33 @@
 const inputText = document.querySelector("input");
+const factorialButton = document.querySelector("button");
+factorialButton.disabled = true;
 const outputText = document.querySelector(".output");
 
 inputText.addEventListener("keyup", () => {
   filterNumber = inputText.value.replace(/[^-?0-9]/gi, "");
   console.log(filterNumber);
   outputText.style.display = "none";
+  if (filterNumber) {
+    return (factorialButton.disabled = false);
+  }
+  factorialButton.disabled = true;
 });
 
-const factorialButton = document.querySelector("button");
 factorialButton.addEventListener("click", () => {
-  console.log("clicked");
+  // console.log("clicked");
   outputText.style.display = "block";
+  outputText.style.marginTop = "10px";
 
   const number = parseInt(filterNumber);
   if (number < 0) {
-    console.log("Error! Factorial for negative number does not exist.");
     return (outputText.innerHTML = `Factorial for <span>${number}</span> never exist.`);
   } else if (number === 0) {
-    console.log(`The factorial of ${number} is 1.`);
     return (outputText.innerHTML = `Factorial for ${number} is <span>'1'</span>.`);
   } else {
     let fact = 1;
     for (i = 1; i <= number; i++) {
       fact *= i;
     }
-    console.log(`The factorial of ${number} is ${fact}.`);
     return (outputText.innerHTML = `<span>'${fact}'</span> is ther factorial value of ${number}`);
   }
 });
